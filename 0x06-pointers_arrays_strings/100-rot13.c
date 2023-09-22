fccde87b32c2f446721d005a5df363d2fb8767b7
alaadirarlx <alaadirar@icloud.com>
2023-09-21 18:52:01 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rot13 - a function that encodes a string using rot13
+ *
+ * @s: input string
+ *
+ * Return: String converted to rot13
+ *
+ */
+
+char *rot13(char *s)
+{
+	int index, count;
+
+	char minus[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char mayus[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (index = 0; s[index] != '\0'; ++index)
+	{
+		for (count = 0; minus[count] != '\0' ; count++)
+		{
+			if (s[index] == minus[count])
+			{
+				s[index] = mayus[count];
+				break;
+			}
+		}
+	}
+	s[index] = '\0';
+	return (s);
+}
